bc71f9c19ea37d36fd143707df7da8d55aac24c9
Sugambaj7 <sugambaj123@gmail.com>
2023-05-21 19:07:30 +0545

    JSX Fragment

--- src/App.js
+++ src/App.js
@@ -6,9 +6,15 @@ function MyButton() {
 
 export default function MyApp() {
   return (
-    <div>
-      <h1>Welcome to my app</h1>
-      <MyButton />
-    </div>
+    <>
+      <div>
+        <h1>Welcome to my app</h1>
+        <MyButton />
+      </div>
+
+      <div>
+        <h2>JSX fragment allows to render multiple elements</h2>
+      </div>
+    </>
   );
 }
