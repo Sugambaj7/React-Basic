5d629757d4563655153b4aba44020cde48598fab
Sugambaj7 <sugambaj123@gmail.com>
2023-05-25 20:32:22 +0545

    created profile component in react

--- src/App.js
+++ src/App.js
@@ -1,52 +1,25 @@
 import "./App.css";
 import "./avatar.css";
-import { useState } from "react";
 
-const user = {
-  name: "John Doe",
-  imageUrl: "https://i.imgur.com/WqDRezM.jpeg",
-  imageSize: 90,
-};
+function Profile() {
+  return (
+    <img
+      src="https://i.imgur.com/MG0wxVE.jpeg"
+      height="300px"
+      width="300px"
+      alt="doraemon"
+    />
+  );
+}
 
 export default function MyApp() {
-  const [count, setCount] = useState(0);
-  function MyButton({ count, onClick }) {
-    return (
-      <>
-        <div>
-          <button onClick={onClick}>Clicked {count} times!!!</button>
-        </div>
-      </>
-    );
-  }
-  function handleClick(event) {
-    setCount(count + 1);
-  }
-
   return (
     <>
-      <div>
-        <h1>Welcome to my app</h1>
-        <MyButton count={count} onClick={handleClick} />
-        <MyButton count={count} onClick={handleClick} />
-      </div>
-
-      <div>
-        <h2>JSX fragment allows to render multiple elements</h2>
-      </div>
-
-      <div>
-        <h3>{user.name}</h3>
-        <img
-          className="avatar"
-          src={user.imageUrl}
-          alt={"Photo of " + user.name}
-          style={{
-            width: user.imageSize,
-            height: user.imageSize,
-          }}
-        />
-      </div>
+      <h2>Doraemon</h2>
+      <section>
+        <Profile />
+        <Profile />
+      </section>
     </>
   );
 }
