bc5349c6ee0db8dfbb9c8a23c6257ae78c8f3ca3
Sugambaj7 <sugambaj123@gmail.com>
2023-05-31 19:37:01 +0545

    Javascript in jsx with curly braces

--- src/App.js
+++ src/App.js
@@ -1,25 +1,32 @@
 import "./App.css";
 import "./avatar.css";
 
-function Profile() {
-  return (
-    <img
-      src="https://i.imgur.com/MG0wxVE.jpeg"
-      height="300px"
-      width="300px"
-      alt="doraemon"
-    />
-  );
-}
+const person = {
+  name: "Sugam Bajracharya",
+  age: 21,
+  theme: {
+    backgroundColor: "pink",
+    color: "white",
+  },
+};
 
-export default function MyApp() {
+export default function TodoList() {
   return (
-    <>
-      <h2>Doraemon</h2>
-      <section>
-        <Profile />
-        <Profile />
-      </section>
-    </>
+    <div style={person.theme}>
+      <h1>{person.name} Todo List</h1>
+      <img
+        className="avatar"
+        src="https://i.imgur.com/cdBxQfX.gif"
+        alt="user"
+        srcset=""
+      />
+
+      <ul>
+        <li>To go to workout</li>
+        <li>To go to work</li>
+        <li>To do assignments</li>
+        <li>To watch movies</li>
+      </ul>
+    </div>
   );
 }
